298f9d30049a6d7b006f3a6883873762e313550c
ukhov79 <ukhov@mati.ru>
2021-06-16 19:07:14 +0300

    It doesnt work today...

--- start.py
+++ start.py
@@ -31,47 +31,6 @@ objpointsr = []
 imgpointsr = []
 
 
-def depth_map(imgL, imgR):
-    """ Depth map calculation. Works with SGBM and WLS. Need rectified images, returns depth map ( left to right disparity ) """
-    # SGBM Parameters -----------------
-    window_size = 3  # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
-
-    left_matcher = cv2.StereoSGBM_create(
-        minDisparity=-1,
-        numDisparities=5*16,  # max_disp has to be dividable by 16 f. E. HH 192, 256
-        blockSize=window_size,
-        P1=8 * 3 * window_size,
-        # wsize default 3; 5; 7 for SGBM reduced size image; 15 for SGBM full size image (1300px and above); 5 Works nicely
-        P2=32 * 3 * window_size,
-        disp12MaxDiff=12,
-        uniquenessRatio=10,
-        speckleWindowSize=50,
-        speckleRange=32,
-        preFilterCap=63,
-        mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
-    )
-    right_matcher = cv2.ximgproc.createRightMatcher(left_matcher)
-    # FILTER Parameters
-    lmbda = 80000
-    sigma = 1.3
-    visual_multiplier = 6
-
-    wls_filter = cv2.ximgproc.createDisparityWLSFilter(matcher_left=left_matcher)
-    wls_filter.setLambda(lmbda)
-
-    wls_filter.setSigmaColor(sigma)
-    displ = left_matcher.compute(imgL, imgR)  # .astype(np.float32)/16
-    dispr = right_matcher.compute(imgR, imgL)  # .astype(np.float32)/16
-    displ = np.int16(displ)
-    dispr = np.int16(dispr)
-    filteredImg = wls_filter.filter(displ, imgL, None, dispr)  # important to put "imgL" here!!!
-
-    filteredImg = cv2.normalize(src=filteredImg, dst=filteredImg, beta=0, alpha=255, norm_type=cv2.NORM_MINMAX);
-    filteredImg = np.uint8(filteredImg)
-
-    return filteredImg
-
-
 def main():
     # Start Left Camera
     left_camera = Camera()
@@ -182,12 +141,6 @@ def main():
             left_image = cv2.remap(left_image, leftMapX, leftMapY, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
             rightMapX, rightMapY = cv2.initUndistortRectifyMap(cm2, dc2, r2, p2, (height, width), cv2.CV_32FC1)
             right_image = cv2.remap(right_image, rightMapX, rightMapY, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
-
-            # Convert to grayscale
-            grayl = cv2.cvtColor(left_image, cv2.COLOR_RGB2GRAY)
-            grayr = cv2.cvtColor(right_image, cv2.COLOR_RGB2GRAY)
-            # Get the disparity map into right image
-            right_image = depth_map(grayl, grayr)
 
         camera_images = np.hstack((left_image, right_image))
         cv2.imshow("IMX219-83 Stereo camera", camera_images)
